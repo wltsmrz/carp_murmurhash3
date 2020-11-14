27d324890d73acf71fe8f17447369184dc395a73
wltsmrz <wlt@smrz.dev>
2020-11-14 21:01:44 +0000

    Fixins

--- src/carp_murmurhash3.h
+++ src/carp_murmurhash3.h
@@ -1,39 +1,32 @@
 #include "murmurhash3.h"
 
 Uint32 murmur3_32x1(const String* key, int seed) {
-  Array chars = String_chars(key);
+  Array bytes = String_to_MINUS_bytes(key);
   uint32_t out;
-  size_t len = strlen(chars.data);
 
-  MurmurHash3_x86_32(chars.data, len, seed, &out);
-
-  CARP_FREE(chars.data);
+  MurmurHash3_x86_32(bytes.data, bytes.len, seed, &out);
 
   return out;
 }
 
 Array murmur3_32x4(const String* key, int seed) {
-  Array chars = String_chars(key);
+  Array bytes = String_to_MINUS_bytes(key);
   Array out = { .len = 4, .capacity = 4, .data  = (uint32_t*)malloc(4 * sizeof(uint32_t)) };
 
-  size_t len = strlen(chars.data);
+  MurmurHash3_x86_128(bytes.data, bytes.len, seed, out.data);
 
-  MurmurHash3_x86_128(chars.data, len, seed, out.data);
-
-  CARP_FREE(chars.data);
+  CARP_FREE(bytes.data);
 
   return out;
 }
 
 Array murmur3_64x2(const String* key, int seed) {
-  Array chars = String_chars(key);
+  Array bytes = String_to_MINUS_bytes(key);
   Array out = { .len = 2, .capacity = 2, .data  = (uint64_t*)malloc(2 * sizeof(uint64_t)) };
 
-  size_t len = strlen(chars.data);
+  MurmurHash3_x64_128(bytes.data, bytes.len, seed, out.data);
 
-  MurmurHash3_x64_128(chars.data, len, seed, out.data);
-
-  CARP_FREE(chars.data);
+  CARP_FREE(bytes.data);
 
   return out;
 }
