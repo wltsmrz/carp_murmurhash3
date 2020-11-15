f312925fcff7d14e636eb7f67453c33d1cefbdd7
wltsmrz <wlt@smrz.dev>
2020-11-15 08:30:48 +0000

    Add to-hex, add tests

--- README.md
+++ README.md
@@ -3,12 +3,22 @@
 Carp bindings for murmurhash3
 
 ```clojure
-(load "https://github.com/wltsmrz/carp_murmurhash3@v0.2.1")
+(load "https://github.com/wltsmrz/carp_murmurhash3@v0.3.0")
 
 (defn-do main []
+  ; 32 bit hash, returns uint32
  (IO.println &(Uint32.str (Murmur3.hash-32x1 "whatever" 90210)))
+  ; 128 bit hash, returns array of four uint32
  (IO.println &(Array.str &(Murmur3.hash-32x4 "whatever" 90210)))
+  ; 128 bit hash, returns array of 2 uint64
  (IO.println &(Array.str &(Murmur3.hash-64x2 "whatever" 90210)))
+
+  ; 32 bit hash, returns hex string
+ (IO.println &(Murmur3.hash-32x1-hex "whatever" 90210))
+  ; 128 bit hash, returns hex string
+ (IO.println &(Murmur3.hash-32x4-hex "whatever" 90210))
+  ; 128 bit hash, returns hex string
+ (IO.println &(Murmur3.hash-64x2-hex "whatever" 90210))
 )
 ```
 
--- src/carp_murmurhash3.h
+++ src/carp_murmurhash3.h
@@ -1,32 +1,55 @@
 #include "murmurhash3.h"
 
-Uint32 murmur3_32x1(const String* key, int seed) {
-  Array bytes = String_to_MINUS_bytes(key);
-  uint32_t out;
+String murmur3_32_to_hex(Uint32* hash) {
+  size_t size = (sizeof(char) * 8) + 1;
+  String buf = CARP_MALLOC(size);
+  snprintf(buf, size, "%08x", *hash);
+  return buf;
+}
 
-  MurmurHash3_x86_32(bytes.data, bytes.len, seed, &out);
+String murmur3_128_to_hex(const Array* hash) {
+  size_t size = (sizeof(char) * 32) + 1;
+  String buf = CARP_MALLOC(size);
+  uint32_t* p = (uint32_t*)hash->data;
+  snprintf(buf, size, "%08x%08x%08x%08x", *p, *(p+1), *(p+2), *(p+3));
+  return buf;
+}
 
+Uint32 murmur3_32x1(const Array* key, int seed) {
+  uint32_t out = 0;
+  MurmurHash3_x86_32(key->data, key->len, seed, &out);
   return out;
 }
 
-Array murmur3_32x4(const String* key, int seed) {
-  Array bytes = String_to_MINUS_bytes(key);
+Array murmur3_32x4(const Array* key, int seed) {
   Array out = { .len = 4, .capacity = 4, .data  = (uint32_t*)malloc(4 * sizeof(uint32_t)) };
-
-  MurmurHash3_x86_128(bytes.data, bytes.len, seed, out.data);
-
-  CARP_FREE(bytes.data);
-
+  MurmurHash3_x86_128(key->data, key->len, seed, out.data);
   return out;
 }
 
-Array murmur3_64x2(const String* key, int seed) {
-  Array bytes = String_to_MINUS_bytes(key);
-  Array out = { .len = 2, .capacity = 2, .data  = (uint64_t*)malloc(2 * sizeof(uint64_t)) };
-
-  MurmurHash3_x64_128(bytes.data, bytes.len, seed, out.data);
-
-  CARP_FREE(bytes.data);
-
+Array murmur3_64x2(const Array* key, int seed) {
+  Array out = { .len = 4, .capacity = 4, .data  = (uint64_t*)malloc(2 * sizeof(uint64_t)) };
+  MurmurHash3_x64_128(key->data, key->len, seed, out.data);
   return out;
 }
+
+String murmur3_32x1_hex(const Array* key, int seed) {
+  Uint32 hash = murmur3_32x1(key, seed);
+  String hex = murmur3_32_to_hex(&hash);
+  return hex;
+}
+
+String murmur3_32x4_hex(const Array* key, int seed) {
+  Array hash = murmur3_32x4(key, seed);
+  String hex = murmur3_128_to_hex(&hash);
+  CARP_FREE(hash.data);
+  return hex;
+}
+
+String murmur3_64x2_hex(const Array* key, int seed) {
+  Array hash = murmur3_64x2(key, seed);
+  String hex = murmur3_128_to_hex(&hash);
+  CARP_FREE(hash.data);
+  return hex;
+}
+
